d9246ef6500085b111ba3e5a130bd1ed14f48665
John-Kimani <kimwaweru100@gmail.com>
2022-04-09 14:59:25 +0300

    Dev: validates user registration

--- members/views.py
+++ members/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm 
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import MemberRegisterForm
 
 def register(request):
     '''
     User registration view
     '''
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = MemberRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created {username}')
+            return redirect('homepage')
+    else:
+        form = MemberRegisterForm()
     return render(request, 'members/register.html', {"form":form})
