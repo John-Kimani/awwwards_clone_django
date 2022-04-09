4d65ab17c67033c56bb6b862af2fbd0a659e7633
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 00:30:49 +0300

    Dev:Create local db profile model

--- members/models.py
+++ members/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
-# Create your models here.
+
+class Profile(models.Model):
+    member = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = CloudinaryField('images/')
+    member_bio = models.TextField(max_length=50, blank=True)
+    date_joined = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
