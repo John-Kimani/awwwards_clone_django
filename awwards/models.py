6c61bc5754095dd11b5aef87b6aa96e0301b49f1
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 03:07:53 +0300

    Creates project class

--- awwards/models.py
+++ awwards/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from members.models import Profile
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+class Projects(models.Model):
+    '''
+    Class that handles project instances
+    '''
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='author')
+    title = models.CharField(max_length=50)
+    article = models.CharField(max_length=500)
+    link = models.URLField(max_length=200)
+    image = CloudinaryField('images/')
+    pub_date = models.DateField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.title
+
