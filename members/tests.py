93ed5461e4b0ee8e26680fb77acfc32b22a2087e
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 03:15:00 +0300

    Test: Members classes

--- members/tests.py
+++ members/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Create your tests here.
+
+class ProfileTestClass(TestCase):
+    '''
+    Test class for profile model
+    '''
+    def setUp(self):
+        self.user = User(username='kimperria', email='kimperriatest@gmail.com')
+        self.profile = Profile(nickname='grand_muller', member_bio='Greatness requires internal toughness')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.user, User))
+        self.assertTrue(isinstance(self.profile, Profile))
