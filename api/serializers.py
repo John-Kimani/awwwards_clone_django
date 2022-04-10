8382a5c8cb062f4b6d9f6f15f2aecc0a280012a6
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 18:42:17 +0300

    Dev:Create profile serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from awwards.models import Projects
+from members.models import Profile
 
 
 class ProjectsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Projects
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
         fields = '__all__'
