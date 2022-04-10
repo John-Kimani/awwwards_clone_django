092e3a82c51d7483b7f7c3188c578b43672bc601
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 03:19:01 +0300

    Test: main app classes

--- awwards/tests.py
+++ awwards/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from .models import Projects
 
-# Create your tests here.
+
+class ProjectTestClass(TestCase):
+    '''
+    Class that tests Project model
+    '''
+    def setUp(self):
+        self.project = Projects(title='Showman', article='This is the greatest show')
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Projects))
