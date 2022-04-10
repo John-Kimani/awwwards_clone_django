d1fde482f3317c70ed7fe0ac032a6403ff5f0ddd
John-Kimani <kimwaweru100@gmail.com>
2022-04-10 14:44:00 +0300

    Dev: Create serializer to save objects from frontend

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('get/', views.getData, name='get'),
+    path('post/', views.postData, name='post'),
 ]
--- api/views.py
+++ api/views.py
@@ -7,8 +7,19 @@ from .serializers import ProjectsSerializer
 @api_view(['GET'])
 def getData(request):
     '''
-    API view functions for get request
+    API view function for get project data
     '''
     projects = Projects.objects.all()
     serializer = ProjectsSerializer(projects, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def postData(request):
+    '''
+    API view function to post project data
+    '''
+    serializer = ProjectsSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
     return Response(serializer.data)
